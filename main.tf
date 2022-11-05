1c2c9d9b95383b0dacada4057fdfa6b2506b6bad
toyosy012 <toyosy012@gmail.com>
2022-11-05 17:21:41 +0900

    feat: feature/#3_add_cluster_code GKEノードプール追加
    クラスターノードプール追加
    
    3つのノードを設定
    ノードをプライベートに変更
    gke用ネットワークを追加

--- main.tf
+++ main.tf
@@ -16,10 +16,86 @@ resource "google_project_iam_member" "gke_provisioning_roles" {
 }
 
 resource "google_container_cluster" "training_cluster" {
-  name     = "training_cluster"
+  name     = "training-cluster"
   location = "asia-northeast1"
 
   remove_default_node_pool = true
   initial_node_count       = 1
   min_master_version = "1.23"
+
+  network = google_compute_network.gke_network.name
+  subnetwork = google_compute_subnetwork.gke_subnetwork.name
+
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes = true
+    master_ipv4_cidr_block = "192.168.0.0/28"
+  }
+
+  master_authorized_networks_config {
+  }
+  ip_allocation_policy {
+    cluster_secondary_range_name = google_compute_subnetwork.gke_subnetwork.secondary_ip_range.0.range_name
+    services_secondary_range_name = google_compute_subnetwork.gke_subnetwork.secondary_ip_range.1.range_name
+  }
+}
+
+resource "google_container_node_pool" "training_nodes" {
+  cluster = google_container_cluster.training_cluster.name
+  name = "training-node-pool"
+
+  location = "asia-northeast1"
+  node_count = 1
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+    service_account = google_service_account.gke_nodes.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  management {
+    auto_repair = true
+    auto_upgrade = true
+  }
+  upgrade_settings {
+    max_surge       = 1
+    max_unavailable = 0
+  }
+}
+
+resource "google_service_account" "gke_nodes" {
+  account_id   = "gke-nodes"
+  display_name = "My Service Account For My Cluster Nodes"
+}
+
+resource "google_compute_network" "gke_network" {
+  name                    = "training-gke-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "gke_subnetwork" {
+  name   = "training-gke-subnetwork"
+  region = "asia-northeast1"
+
+  # サブネットで使用したい内部IPアドレスの範囲を指定する
+  ip_cidr_range = "10.0.0.0/16"
+  network       = google_compute_network.gke_network.self_link
+
+  # CloudLoggingにFlowLogログを出力したい場合は設定する
+  log_config {
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+
+  secondary_ip_range {
+    range_name    = "training-gke-subnet-for-pods"
+    ip_cidr_range = "10.1.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "training-gke-subnet-for-services"
+    ip_cidr_range = "10.2.0.0/16"
+  }
+
+  private_ip_google_access = true
 }
